0a14f2250a38ca8eb2a61cfbd920d6c35a6b0bc9
Ilya Dzeraziak <ilyadzeraziak@gmail.com>
2017-10-12 16:31:03 +0300

    Added Json and parse of it

--- bin/main.py
+++ bin/main.py
@@ -1,14 +1,22 @@
 import cv2 as cv
-
-
-
+import json
 
 # Color
 RED = 0, 0, 255
 
-#State loc
+# json_loc
+json_loc = "data/json/loc.json"
 
-loc = {}
+# location reading from json
+
+def location(Name_of_state):
+   f = open(json_loc)
+   s = f.read()
+   location = json.loads(s, parse_int=1)
+   return int(location[Name_of_state]['x']), int(location[Name_of_state]['y'])
+
+
+
 
 # Map img ref
 map_img = 'data/map.jpg'
@@ -16,15 +24,15 @@ map_img = 'data/map.jpg'
 read = cv.imread(map_img)
 
 # loc of mark
-y = int(input("Y: "))
-x = int(input("X: "))
+#y = int(input("Y: "))
+#x = int(input("X: "))
 
-loc = y, x
+#loc = y, x
 # Y - fist var; X - second var;
-read = cv.circle(read, loc, 10, RED, -1)
+read = cv.circle(read, location('Oregon'), 10, RED, -1)
 
 
-screen = cv.imshow("ss", read)
+screen = cv.imshow("Storm Map ", read)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
