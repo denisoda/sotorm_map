854d7167534518cd1d0ebc359b9da216c23b4df5
Ilya Dzeraziak <ilyadzeraziak@gmail.com>
2017-10-12 14:41:21 +0300

    Fix folders and added pic render logic

--- bin/main.py
+++ bin/main.py
@@ -1,8 +1,30 @@
 import cv2 as cv
 
+
+
+
+# Color
+RED = 0, 0, 255
+
+#State loc
+
+loc = {}
+
+# Map img ref
 map_img = 'data/map.jpg'
 
 read = cv.imread(map_img)
 
-cv.imshow()
+# loc of mark
+y = int(input("Y: "))
+x = int(input("X: "))
+
+loc = y, x
+# Y - fist var; X - second var;
+read = cv.circle(read, loc, 10, RED, -1)
+
+
+screen = cv.imshow("ss", read)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
