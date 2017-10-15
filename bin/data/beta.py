17fe10800d38b845dfd5a5136b548c8b64a88501
Ilya Dzeraziak <ilyadzeraziak@gmail.com>
2017-10-15 17:49:16 +0300

    Manipualtion with strings and converting it to int

--- bin/data/beta.py
+++ bin/data/beta.py
@@ -5,7 +5,7 @@ for file in range(43, 61):
  with open('csv/'+ str(file) +'.csv' ) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-      print(row['YEARMONTH'], row['LOCATION'])
+      print(int(str(row['YEARMONTH'][:-2])), row['LOCATION'])
 
 
 
