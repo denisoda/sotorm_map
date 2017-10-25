bdbaeee07e29c64727d90d9344557e1742a78870
Ilya Dzeraziak <ilya.by@bk.ru>
2017-10-26 02:42:24 +0300

    Fixing bugs and creating new one :)

--- bin/data/1`23.py
+++
@@ -1,7 +0,0 @@
-a = ["a","b","c"]
-
-a.pop(0)
-
-
-for i in a:
- print(a,a.__len__())
--- bin/parse.py
+++ bin/parse.py
@@ -1,24 +1,5 @@
 import csv
 
-def csv_parse(location = 'data/csv/', i=43, b=61):
- for file in range(i, b):
-  with open(location + str(file) + '.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-      print(int(str(row['YEARMONTH'][:-2])), row['LOCATION'])
-
-
-def csv_parse_row(year,location = 'data/csv/', i=43, b=62,row1 = 'YEARMONTH', row2 = 'LATITUDE', row3 = 'LONGITUDE',arr_date = [], arr_x = [], arr_y =[] ):
- for file in range(i, b):
-     with open(location + str(file) + '.csv') as csvfile:
-      reader = csv.DictReader(csvfile)
-      for row in reader:
-       int(str(row[row1][:-2])), row[row2], row[row3]
-       if(int(str(row[row1][:-2])) == year):
-        return int(str(row[row1][:-2])), row[row2], row[row3]
-     if(int(str(row[row1][:-2])) > year):
-         break
-
 def csv_parse_row(year,location = 'data/csv/', i=43, b=62,row1 = 'YEARMONTH', row2 = 'LATITUDE', row3 = 'LONGITUDE',arr_date = [], arr_x = [], arr_y =[] ):
   for file in range(i, b):
     with open(location + str(file) + '.csv') as csvfile:
@@ -32,7 +13,7 @@ def csv_parse_row(year,location = 'data/csv/', i=43, b=62,row1 = 'YEARMONTH', ro
          arr_y.append(str(row[row3]))
     if(int(str(row[row1][:-2])) > year):
         return   arr_date, arr_x, arr_y
-        break
+
 
 
 
